df8f83f6a7eef237dc94813d539b939cabfcd05f
praisethesun227 <praisethesun227@gmail.com>
2023-11-25 06:32:35 +0300

    fix: fix deselection of selected item on deleting another item

--- src/app.js
+++ src/app.js
@@ -28,7 +28,10 @@ function App({store}) {
                 <div className='Item-code'>{item.code}</div>
                 <div className='Item-title'>{item.title + buildSelectionsCountString(item.selectionsCount)}</div>
                 <div className='Item-actions'>
-                  <button onClick={() => store.deleteItem(item.code)}>
+                  <button onClick={(e) => {
+                      e.stopPropagation();
+                      store.deleteItem(item.code)
+                    }}>
                     Удалить
                   </button>
                 </div>
