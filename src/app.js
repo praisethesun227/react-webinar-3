3ae1c07a8be381f0a209b2feb2f380eb5f0a4ea4
praisethesun227 <praisethesun227@gmail.com>
2023-11-29 22:53:41 +0300

    feat: remove add item feature

--- src/app.js
+++ src/app.js
@@ -21,16 +21,12 @@ function App({store}) {
     onSelectItem: useCallback((code) => {
       store.selectItem(code);
     }, [store]),
-
-    onAddItem: useCallback(() => {
-      store.addItem();
-    }, [store])
   }
 
   return (
     <PageLayout>
       <Head title='Приложение на чистом JS'/>
-      <Controls onAdd={callbacks.onAddItem}/>
+      <Controls/>
       <List list={list}
             onDeleteItem={callbacks.onDeleteItem}
             onSelectItem={callbacks.onSelectItem}/>
--- src/components/controls/index.js
+++ src/components/controls/index.js
@@ -2,20 +2,11 @@ import React from "react";
 import PropTypes from 'prop-types';
 import './style.css';
 
-function Controls({onAdd}) {
+function Controls() {
   return (
     <div className='Controls'>
-      <button onClick={() => onAdd()}>Добавить</button>
     </div>
   )
-}
-
-Controls.propTypes = {
-  onAdd: PropTypes.func
-};
-
-Controls.defaultProps = {
-  onAdd: () => {}
 }
 
 export default React.memo(Controls);
